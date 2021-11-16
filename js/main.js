df5271179a1603409c641adb88e322f7b55d326d
Job <jobtalle@hotmail.com>
2021-11-16 21:13:04 +0100

    Tweaks

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ const context = renderer.getContext("2d");
 const radius = 777;
 const scale = Math.min(width, height) / (radius * 2);
 const hc = .6;
-const rf = .4 + .2 * random.getFloat();
-const dotChance = .01 * random.getFloat();
+const rf = .4 + .2 * Math.pow(random.getFloat(), 2.2);
+const dotChance = .005 + .01 * Math.pow(random.getFloat(), 1.9);
 const gradient = context.createRadialGradient(
     width * .5 / scale,
     height * hc / scale,
@@ -20,14 +20,13 @@ const gradient = context.createRadialGradient(
     width * .5 / scale,
     height * .3 / scale,
     Math.sqrt(radius * radius + radius * radius) * 1.2);
-
 const hue1 = random.getFloat() * 360;
 const hue2 = (hue1 + random.getFloat() * 280 + 40) % 360;
 const s = "40%";
 const l = "38%";
 
 gradient.addColorStop(0, "hsl(" + Math.round(hue1).toString() + ",50%," + l + ")");
-gradient.addColorStop(.003, "hsl(" + Math.round(hue2).toString() + "," + s + ",20%)");
+gradient.addColorStop(.0015, "hsl(" + Math.round(hue2).toString() + "," + s + ",20%)");
 gradient.addColorStop(1, "hsl(" + Math.round(hue2).toString() + "," + s + ",1%)");
 
 context.scale(scale, scale);
@@ -35,13 +34,12 @@ context.fillStyle = gradient;
 context.fillRect(0, 0, width / scale, height / scale);
 context.translate(width * .5 / scale, height * hc / scale);
 
-const dropletCount = 400;
-const steps = 6000;
+const dropletCount = 600;
+const steps = 800;
 const speed = 20;
 const noiseScale = .0047;
 const ox = radius * 8 * random.getFloat();
 const oy = radius * 8 * random.getFloat();
-
 const xFlow = cubicNoiseConfig(random.getFloat());
 const yFlow = cubicNoiseConfig(random.getFloat());
 const droplets = [];
@@ -59,7 +57,7 @@ for (let i = 0; i < dropletCount; ++i) {
 const overlay = context.createRadialGradient(0, 0, 0, 0, radius * -.05, radius * rf * 1.1);
 
 overlay.addColorStop(0, "rgba(255,255,255,0)");
-overlay.addColorStop(.85, "rgba(255,255,255,0.34)");
+overlay.addColorStop(.8, "rgba(255,255,255,0.34)");
 overlay.addColorStop(1, "#ffffff");
 
 context.globalCompositeOperation = "lighten";
@@ -112,7 +110,13 @@ for (const droplet of droplets) {
 }
 
 context.globalCompositeOperation = "source-over";
-context.fillStyle = "rgba(255,255,255,0.42)";
+context.fillStyle = "rgba(255,255,255,.42)";
+context.strokeStyle = "rgba(255,255,255,.4)";
+
+context.lineWidth = 3;
+context.beginPath();
+context.arc(0, 0, radius * rf, 0, Math.PI * 2);
+context.stroke();
 
 for (const dot of dots) {
     context.beginPath();
